48121241b28768e16714a4b62230833a926a62bc
seung-heee <tmdgmldi2595@naver.com>
2024-03-15 02:35:18 +0900

    Init: Spring 초기세팅

---
+++ src/main/java/com/example/CheerupYouth_Back/CheerupYouthBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.CheerupYouth_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerupYouthBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheerupYouthBackApplication.class, args);
+	}
+
+}
